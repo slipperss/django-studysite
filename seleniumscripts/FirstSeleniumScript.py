af38d683c39edc06a0923f6c776a81a30c59952b
slippers <ks.kvgg234@gmail.com>
2022-06-23 16:14:01 +0300

    *

---
+++ seleniumscripts/FirstSeleniumScript.py
@@ -0,0 +1,53 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+import time
+
+options = Options()
+
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
+
+
+driver=webdriver.Chrome(executable_path="C:\\Users\\BRBCO\\Downloads\\chromedriver_win32\\chromedriver.exe",
+                        chrome_options=options)
+
+
+
+
+#ютуб
+driver.get("http://127.0.0.1:8000/youtube")
+driver.find_element_by_name("text").send_keys("Selenium")
+driver.find_element_by_class_name("btn").click()
+
+time.sleep(0.3)
+#вики
+driver.get("http://127.0.0.1:8000/wiki")
+driver.find_element_by_name("text").send_keys("Selenium")
+driver.find_element_by_class_name("btn").click()
+
+time.sleep(0.3)
+
+#книги
+driver.get("http://127.0.0.1:8000/books")
+driver.find_element_by_name("text").send_keys("Selenium")
+driver.find_element_by_class_name("btn").click()
+
+time.sleep(0.3)
+
+#todo
+driver.get("http://127.0.0.1:8000/todo")
+driver.find_element_by_name("title").send_keys("Walk daily")
+driver.find_element_by_class_name("btn").click()
+
+
+#auth
+driver.get("https://127.0.0.8000/login")
+driver.find_element_by_name("username").send_keys("root")
+driver.find_element_by_name("password").send_keys(1234)
+driver.find_element_by_class_name("btn").click()
+
+time.sleep(0.3)
+
+
